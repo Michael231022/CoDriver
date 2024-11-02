2c2e06ad20236a9b4c12effc52c04d6c6f390f33
Mic <56187299+Michael231022@users.noreply.github.com>
2024-11-03 00:06:25 +0800

    Delete Website directory

--- Website/user_data/user_data.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $username = $_POST['Username'];
-    $email = $_POST['Email'];
-    $user_password = $_POST['User_password'];
-    $gender = $_POST['Gender'];
-    $email = $_POST['Email'];
-    $Birth = $_POST['Birth'];
-?>
